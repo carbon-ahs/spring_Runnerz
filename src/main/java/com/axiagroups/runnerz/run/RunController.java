9ca96c5708afe7b80f5583b448f2886c4d9d51e6
Ahsan Habib <ict.wsd19@waltonplc.com>
2024-06-10 16:30:28 +0600

    part 3 done

--- src/main/java/com/axiagroups/runnerz/run/RunController.java
+++ src/main/java/com/axiagroups/runnerz/run/RunController.java
@@ -2,9 +2,7 @@ package com.axiagroups.runnerz.run;
 
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -41,7 +39,7 @@ public class RunController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run, id);
     }
 
